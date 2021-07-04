3298ed2d3dab6052e6e185048858aa33daf7f860
ubed1312 <ubedibrahim6@gmail.com>
2021-07-04 08:12:36 +0100

    commit

--- Dashboard/pages/mailbox/read-mail1.php
+++ Dashboard/pages/mailbox/read-mail1.php
@@ -246,7 +246,7 @@ $row1 = mysqli_fetch_assoc($result1);
             <!-- /.card -->
           </div>
           <!-- /.col -->
-
+      
        <div class="col-md-9">
           <div class="card card-primary card-outline">
             <div class="card-header">
