67349f3077b7aa5ddbcafc02e271d272efcb2b0b
ubed1312 <ubedibrahim6@gmail.com>
2021-07-04 03:41:01 +0100

    commit

--- att_travail.php
+++ att_travail.php
@@ -9,19 +9,20 @@ $dbname = "siteweb";
 //connexion au serveur
 $con = mysqli_connect($servername, $username, $password, $dbname);
 
-if(isset($_POST['Envoyer2'])){
+if(isset($_POST['Envoyer1'])){
 
+
+
+    
      @$nom = $_POST['nom'];
      @$prenom = $_POST['prenom'];
      @$cin = $_POST['cin'];
      @$departement = $_POST['departement'];
-     @$salle = $_POST['salle'];
-    //  @$date_salle = date("Y-m-d H:i:s");
-     @$date1= strtr($_REQUEST['date_salle'], '/', '-');
-     @$date= date('Y-m-d', strtotime($date1));
+     @$mail = $_POST['mail'];
+     
 
 
-    $sql="INSERT INTO sall (nom,prenom,cin,departement,salle,date_salle) VALUES('$nom','$prenom','$cin','$departement','$salle',$date_salle)";
+    $sql="INSERT INTO att_travail (nom,prenom,cin,departement,mail) VALUES('$nom','$prenom','$cin','$departement','$mail')";
     $result= $con->query($sql);
     header("location:ensei.html");
 }
