394b03c3fcf244b4a3f8a5c4fac15eca602f1bd5
elmehdiben20 <mehdibenhmidi51@gmail.com>
2021-07-04 15:22:02 +0100

    commit

--- salle.php
+++ salle.php
@@ -17,11 +17,10 @@ if(isset($_POST['Envoyer2'])){
      @$departement = $_POST['departement'];
      @$salle = $_POST['salle'];
     //  @$date_salle = date("Y-m-d H:i:s");
-     @$date1= strtr($_REQUEST['date_salle'], '/', '-');
-     @$date= date('Y-m-d', strtotime($date1));
+     @$date= $_POST['date_salle'];
 
 
-    $sql="INSERT INTO sall (nom,prenom,cin,departement,salle,date_salle) VALUES('$nom','$prenom','$cin','$departement','$salle',$date_salle)";
+    $sql="INSERT INTO sall (nom,prenom,cin,departement,salle,date_salle) VALUES('$nom','$prenom','$cin','$departement','$salle','$date')";
     $result= $con->query($sql);
     header("location:ensei.html");
 }
